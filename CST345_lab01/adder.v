0e9bf922394cf926f2315d55647e1e1319b5a764
Cal <nahallacman@gmail.com>
2015-01-17 01:25:02 -0800

    Initial commit, majors changes to .gitignore
    The only thing that should be pushed to git is source files, the project
    file tends to break when you move it between computers.

---
+++ CST345_lab01/adder.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    01:18:34 01/17/2015 
+// Design Name: 
+// Module Name:    adder 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module adder(
+    input [3:0] a,
+    input [3:0] b,
+    output [3:0] sum,
+    output cout
+    );
+
+
+endmodule
