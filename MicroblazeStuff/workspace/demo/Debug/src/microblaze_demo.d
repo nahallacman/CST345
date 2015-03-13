5c647639bf436b99632f9e01ad639fc71db53663
Cal Barkman <callahan.barkman@gmail.com>
2015-03-12 21:17:32 -0700

    Lab05 done

---
+++ MicroblazeStuff/workspace/demo/Debug/src/microblaze_demo.d
@@ -0,0 +1,36 @@
+src/microblaze_demo.d: ../src/microblaze_demo.c \
+ ../../demo_bsp/microblaze_0/include/xparameters.h \
+ ../../demo_bsp/microblaze_0/include/xgpio.h \
+ ../../demo_bsp/microblaze_0/include/xil_types.h \
+ ../../demo_bsp/microblaze_0/include/xil_assert.h \
+ ../../demo_bsp/microblaze_0/include/xstatus.h \
+ ../../demo_bsp/microblaze_0/include/xbasic_types.h \
+ ../../demo_bsp/microblaze_0/include/xgpio_l.h \
+ ../../demo_bsp/microblaze_0/include/xil_io.h \
+ ../../demo_bsp/microblaze_0/include/mb_interface.h \
+ ../../demo_bsp/microblaze_0/include/xil_printf.h ../src/platform.h \
+ ../src/platform_config.h
+
+../../demo_bsp/microblaze_0/include/xparameters.h:
+
+../../demo_bsp/microblaze_0/include/xgpio.h:
+
+../../demo_bsp/microblaze_0/include/xil_types.h:
+
+../../demo_bsp/microblaze_0/include/xil_assert.h:
+
+../../demo_bsp/microblaze_0/include/xstatus.h:
+
+../../demo_bsp/microblaze_0/include/xbasic_types.h:
+
+../../demo_bsp/microblaze_0/include/xgpio_l.h:
+
+../../demo_bsp/microblaze_0/include/xil_io.h:
+
+../../demo_bsp/microblaze_0/include/mb_interface.h:
+
+../../demo_bsp/microblaze_0/include/xil_printf.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
---
+++ MicroblazeStuff/workspace/demo/Debug/src/platform.d
@@ -0,0 +1,22 @@
+src/platform.d: ../src/platform.c \
+ ../../demo_bsp/microblaze_0/include/xparameters.h \
+ ../../demo_bsp/microblaze_0/include/xil_cache.h \
+ ../../demo_bsp/microblaze_0/include/mb_interface.h \
+ ../../demo_bsp/microblaze_0/include/xbasic_types.h \
+ ../../demo_bsp/microblaze_0/include/xil_types.h \
+ ../../demo_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+../../demo_bsp/microblaze_0/include/xparameters.h:
+
+../../demo_bsp/microblaze_0/include/xil_cache.h:
+
+../../demo_bsp/microblaze_0/include/mb_interface.h:
+
+../../demo_bsp/microblaze_0/include/xbasic_types.h:
+
+../../demo_bsp/microblaze_0/include/xil_types.h:
+
+../../demo_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
